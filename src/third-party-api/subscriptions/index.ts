7ebb6f2b55175cc4258b03a099e4570df9d16e7d
Andrew <aosborne@adaptavist.com>
2022-12-05 13:23:51 +0000

    feat: adding upgrade and downgrade subscription method

--- src/third-party-api/subscriptions/index.ts
+++ src/third-party-api/subscriptions/index.ts
@@ -3,8 +3,22 @@ import { ISubscription } from '../types';
 
 const resourceName = 'subscriptions';
 
+interface IChangePlanConfig {
+  newPlanId: string;
+  subscriptionId: string;
+}
 export default class Subscriptions extends Base {
   getSubscription(subscriptionId: string): Promise<ISubscription[]> {
     return this._request(`/${resourceName}/${subscriptionId}`);
+  }
+
+  changePlan(planConfig: IChangePlanConfig) {
+    return this._request(`/${resourceName}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        newPlanId: planConfig.newPlanId,
+        subscriptionId: planConfig.subscriptionId,
+      }),
+    });
   }
 }
