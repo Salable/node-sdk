074cd99cdeaf81b8bbb51e9ee41b0e6c0091f14d
SasankAVST <sganapathiraju@adaptavist.com>
2024-10-09 11:17:46 -0400

    chore: refactor getUrl function

--- src/utils/get-url.ts
+++ src/utils/get-url.ts
@@ -1,14 +1,7 @@
 function getUrl(url: string, params: Record<string, unknown>) {
     const paramList = Object.entries(params);
     if (!paramList.length) return url;
-    const searchParams = [];
-    for (const [k, v] of Object.entries(params)) {
-        if (Array.isArray(v)) {
-            searchParams.push(`${k}=${v.join(',')}`)
-            continue;
-        }
-        searchParams.push(`${k}=${v}`)
-    }
+    const searchParams = paramList.map(([k, v]) => Array.isArray(v) ? `${k}=${v.join(',')}` : `${k}=${v}`);
     return `${url}?${searchParams.join('&')}`;
 }
 
