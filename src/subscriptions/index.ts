cf1bc9997ced373b7d3c8a0b3ef95d753b58507e
Joshua Okoro <jokoro@adaptavist.com>
2024-10-11 12:04:40 -0500

    chore: extracting query params and typing of response of subscription  method

--- src/subscriptions/index.ts
+++ src/subscriptions/index.ts
@@ -1,69 +1,202 @@
 import { ApiRequest, TVersion, Version } from '..';
 import {
-  CancelWhen,
-  SubscriptionRemoveSeatsOptions,
   Subscription,
-  SubscriptionAddSeatsOptions,
-  SubscriptionsChangePlanOptions,
+  GetAllLicensesResponse,
+  SubscriptionsChangePlan,
+  SubscriptionInvoice,
+  SubscriptionPlan,
+  SubscriptionPaymentLink,
+  SubscriptionPaymentMethod,
+  SubscriptionSeatResponse,
 } from '../types';
 import { v2SubscriptionMethods } from './v2';
 
 export type SubscriptionVersions = {
   [Version.V2]: {
     /**
-     *  Get a single subscription
+     *  Retrieves a list of all subscriptions.
      *
      *  @param {string} subscriptionUuid - The UUID of the subscription
+     *
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptions
+     *
+     * @returns {Promise<GetAllLicensesResponse>} The data of the subscription requested
+     */
+    getAll: (options?: {
+      status?:
+      | 'active'
+      | 'canceled'
+      | 'paused'
+      | 'trailing'
+      | 'deleted'
+      | 'past_due'
+      | 'incomplete';
+      email?: string;
+      cursor?: string;
+      take?: string;
+      expand?: string[];
+    }) => Promise<GetAllLicensesResponse>;
+
+    /**
+     *  Retrieves the subscription data based on the UUID. By default, the response does not contain any relational data. If you want to expand the relational data, you can do so with the `expand` query parameter.
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionByUuid
      *
      * @returns {Promise<Subscription>} The data of the subscription requested
      */
-    getOne: (subscriptionUuid: string) => Promise<Subscription>;
+    getOne: (
+      subscriptionUuid: string,
+      options?: {
+        expand: string[];
+      },
+    ) => Promise<Subscription>;
 
     /**
-     *  Change a subscription's plan
+     *  Changes a subscription's plan based on UUID. If the subscription is usage based the requested subscription will be canceled and a new subscription will be created on the plan you are changing to.
      *
      *  @param {string} subscriptionUuid - The UUID of the subscription
-     *  @param {object} SubscriptionsChangePlanOptions - Change subscription plan options
      *
-     * @returns {Promise<void>}
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/changeSubscriptionsPlan
+     *
+     * @returns {Promise<SubscriptionsChangePlan>}
      */
     changePlan: (
       subscriptionUuid: string,
-      options: SubscriptionsChangePlanOptions,
+      options: {
+        planUuid: string;
+        proration?: string;
+      },
+    ) => Promise<SubscriptionsChangePlan>;
+
+    /**
+     *  Retrieves a list of invoices for a subscription
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionInvoices
+     *
+     * @returns {Promise<SubscriptionInvoice>}
+     */
+    getInvoices: (subscriptionUuid: string) => Promise<SubscriptionInvoice>;
+
+    /**
+     *  Retrieves a list of available plans that a subscribed user can switch to
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionUpdatablePlans
+     *
+     * @returns {Promise<SubscriptionPlan[]>}
+     */
+    getSwitchablePlans: (subscriptionUuid: string) => Promise<SubscriptionPlan[]>;
+
+    /**
+     *  Cancels a subscription by providing the `subscriptionUuid` It will cancel immediately or at the end of the Subscription based on value of the `when` query parameter.
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription to cancel
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/cancelSubscription
+     *
+     * @returns {Promise<void>}
+     */
+    cancel: (
+      subscriptionUuid: string,
+      when: {
+        when?: 'now' | 'end';
+      },
     ) => Promise<void>;
 
     /**
-     *  Cancel's a subscription
+     *  Retrieves the update payment link for a specific subscription. The link opens up a management portal for your payment integration that will have an option for the customer to update their payment details.
      *
      *  @param {string} subscriptionUuid - The UUID of the subscription to cancel
-     *  @param {enum} CancelWhen - when Whether you want to cancel the subscription now or end of cycle
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionUpdatePaymentLink
+     *
+     * @returns {Promise<SubscriptionPaymentLink>}
+     */
+    getUpdatePaymentLink: (subscriptionUuid: string) => Promise<SubscriptionPaymentLink>;
+
+    /**
+     *  Retrieves the customer portal link for a subscription. The link opens up a subscription management portal for your payment integration that will have an options for the customer to manage their subscription.
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription to cancel
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionCustomerPortalLink
+     *
+     * @returns {Promise<SubscriptionPaymentLink>}
+     */
+    getPortalLink: (subscriptionUuid: string) => Promise<SubscriptionPaymentLink>;
+
+    /**
+     *  Retrieves the cancel subscription link for a specific subscription. The link opens up a subscription management portal for your payment integration that will have an option for the customer to cancel the subscription.
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription to cancel
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionCancelLink
+     *
+     * @returns {Promise<SubscriptionPaymentLink>}
+     */
+    getCancelSubscriptionLink: (subscriptionUuid: string) => Promise<SubscriptionPaymentLink>;
+
+    /**
+     *  Retrieves the payment method used to pay for a subscription.
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription to cancel
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionPaymentMethod
+     *
+     * @returns {Promise<SubscriptionPaymentMethod>}
+     */
+    getPaymentMethod: (subscriptionUuid: string) => Promise<SubscriptionPaymentMethod>;
+
+    /**
+     *  Reactivate a Subscription's scheduled cancellation before the billing period has passed. If the billing period has passed and the Subscription has already been canceled please create a new Subscription.
+     *
+     *  @param {string} subscriptionUuid - The UUID of the subscription to cancel
+     *
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/getSubscriptionReactivate
      *
      * @returns {Promise<void>}
      */
-    cancel: (subscriptionUuid: string, when: CancelWhen) => Promise<void>;
+    reactiveSubscription: (subscriptionUuid: string) => Promise<void>;
 
     /**
-     *  Add Seats to a Subscription
+     *  Incrementing will create unassigned licenses.
      *
      *  @param {string} subscriptionUuid - The UUID of the subscription
-     *  @param {object} SubscriptionAddSeatsOptions - Options to be passed in to the add seats method
      *
-     * @returns {Promise<void>}
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/incrementSubscriptionSeats
+     *
+     * @returns {Promise<SubscriptionSeatResponse>}
      */
-    addSeats: (subscriptionUuid: string, options: SubscriptionAddSeatsOptions) => Promise<void>;
+    addSeats: (
+      subscriptionUuid: string,
+      options: {
+        increment: number;
+        proration?: string;
+      },
+    ) => Promise<SubscriptionSeatResponse>;
 
     /**
-     *  Remove Seats from a Subscription
+     *  Decrementing will only remove unassigned licenses.
      *
      *  @param {string} subscriptionUuid - The UUID of the subscription
-     *  @param {object} SubscriptionRemoveSeatsOptions - Options to be passed in to the add seats method
      *
-     * @returns {Promise<void>}
+     * Docs - https://docs.salable.app/api/v2#tag/Subscriptions/operation/decrementSubscriptionSeats
+     *
+     * @returns {Promise<SubscriptionSeatResponse>}
      */
     removeSeats: (
       subscriptionUuid: string,
-      options: SubscriptionRemoveSeatsOptions,
-    ) => Promise<void>;
+      options: {
+        decrement: number;
+        proration?: string;
+      },
+    ) => Promise<SubscriptionSeatResponse>;
   };
 };
 
--- src/subscriptions/v2/index.ts
+++ src/subscriptions/v2/index.ts
@@ -1,66 +1,108 @@
 import { ApiRequest } from '@/src';
 import { SubscriptionVersions } from '..';
 import {
-    CancelWhen,
+    GetAllLicensesResponse,
+    SearchParamOptions,
     Subscription,
-    SubscriptionAddSeatsOptions,
-    SubscriptionRemoveSeatsOptions,
-    SubscriptionsChangePlanOptions,
+    SubscriptionInvoice,
+    SubscriptionPaymentLink,
+    SubscriptionPaymentMethod,
+    SubscriptionPlan,
+    SubscriptionsChangePlan,
+    SubscriptionSeatResponse,
 } from '@/src/types';
 import { RESOURCE_NAMES, SALABLE_BASE_URL } from '@/src/constants';
+import getUrl from '@/src/utils/get-url';
+
+const baseUrl = `${SALABLE_BASE_URL}/${RESOURCE_NAMES.SUBSCRIPTIONS}`;
 
 export const v2SubscriptionMethods = (request: ApiRequest): SubscriptionVersions['v2'] => ({
-    getOne: async (subscriptionUuid: string): Promise<Subscription> => {
-        return request(`${SALABLE_BASE_URL}/${RESOURCE_NAMES.SUBSCRIPTIONS}/${subscriptionUuid}`, {
-            method: 'GET',
-        }) as unknown as Subscription;
-    },
+  getAll: async (options): Promise<GetAllLicensesResponse> => {
+    return request(getUrl(baseUrl, options as SearchParamOptions), {
+      method: 'GET',
+    }) as unknown as GetAllLicensesResponse;
+  },
 
-    changePlan: async (
-        subscriptionUuid: string,
-        config: SubscriptionsChangePlanOptions,
-    ): Promise<void> => {
-        return request(
-            `${SALABLE_BASE_URL}/${RESOURCE_NAMES.SUBSCRIPTIONS}/${subscriptionUuid}/change-plan`,
-            {
-                method: 'PUT',
-                body: JSON.stringify(config),
-            },
-        ) as unknown as void;
-    },
+  getOne: async (subscriptionUuid: string, options): Promise<Subscription> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}`, options as SearchParamOptions), {
+      method: 'GET',
+    }) as unknown as Subscription;
+  },
 
-    cancel: async (subscriptionUuid: string, when: CancelWhen): Promise<void> => {
-        return request(
-            `${SALABLE_BASE_URL}/${RESOURCE_NAMES.SUBSCRIPTIONS}/${subscriptionUuid}/change?when=${when}`,
-            {
-                method: 'PUT',
-            },
-        ) as unknown as void;
-    },
+  changePlan: async (subscriptionUuid: string, options): Promise<SubscriptionsChangePlan> => {
+    return request(
+      getUrl(`${baseUrl}/${subscriptionUuid}/change-plan`, options as SearchParamOptions),
+      {
+        method: 'PUT',
+      },
+    ) as unknown as SubscriptionsChangePlan;
+  },
 
-    addSeats: async (
-        subscriptionUuid: string,
-        options: SubscriptionAddSeatsOptions,
-    ): Promise<void> => {
-        return request(
-            `${SALABLE_BASE_URL}/${RESOURCE_NAMES.SUBSCRIPTIONS}/${subscriptionUuid}/seats`,
-            {
-                method: 'POST',
-                body: JSON.stringify(options),
-            },
-        ) as unknown as void;
-    },
+  getInvoices: async (subscriptionUuid: string): Promise<SubscriptionInvoice> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/invoices`, {}), {
+      method: 'GET',
+    }) as unknown as SubscriptionInvoice;
+  },
 
-    removeSeats: async (
-        subscriptionUuid: string,
-        options: SubscriptionRemoveSeatsOptions,
-    ): Promise<void> => {
-        return request(
-            `${SALABLE_BASE_URL}/${RESOURCE_NAMES.SUBSCRIPTIONS}/${subscriptionUuid}/seats`,
-            {
-                method: 'PUT',
-                body: JSON.stringify(options),
-            },
-        ) as unknown as void;
-    },
+  getSwitchablePlans: async (subscriptionUuid: string): Promise<SubscriptionPlan[]> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/updateplan/listplans`, {}), {
+      method: 'GET',
+    }) as unknown as SubscriptionPlan[];
+  },
+
+  cancel: async (subscriptionUuid: string, options): Promise<void> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/cancel`, options as SearchParamOptions), {
+      method: 'PUT',
+    }) as unknown as void;
+  },
+
+  getUpdatePaymentLink: async (subscriptionUuid: string): Promise<SubscriptionPaymentLink> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/updatepaymentlink`, {}), {
+      method: 'GET',
+    }) as unknown as SubscriptionPaymentLink;
+  },
+
+  getPortalLink: async (subscriptionUuid: string): Promise<SubscriptionPaymentLink> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/customer-portal`, {}), {
+      method: 'GET',
+    }) as unknown as SubscriptionPaymentLink;
+  },
+
+  getCancelSubscriptionLink: async (subscriptionUuid: string): Promise<SubscriptionPaymentLink> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/cancelpaymentlink`, {}), {
+      method: 'GET',
+    }) as unknown as SubscriptionPaymentLink;
+  },
+
+  getPaymentMethod: async (subscriptionUuid: string): Promise<SubscriptionPaymentMethod> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/payment-method`, {}), {
+      method: 'GET',
+    }) as unknown as SubscriptionPaymentMethod;
+  },
+
+  reactiveSubscription: async (subscriptionUuid: string): Promise<void> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/reactivate`, {}), {
+      method: 'PUT',
+    }) as unknown as void;
+  },
+
+  addSeats: async (
+    subscriptionUuid: string,
+    options,
+  ): Promise<SubscriptionSeatResponse> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/seats`, {}), {
+      method: 'POST',
+      body: JSON.stringify(options),
+    }) as unknown as SubscriptionSeatResponse;
+  },
+
+  removeSeats: async (
+    subscriptionUuid: string,
+    options,
+  ): Promise<SubscriptionSeatResponse> => {
+    return request(getUrl(`${baseUrl}/${subscriptionUuid}/seats`, {}), {
+      method: 'PUT',
+      body: JSON.stringify(options),
+    }) as unknown as SubscriptionSeatResponse;
+  },
 });
