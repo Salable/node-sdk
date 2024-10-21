05f6554892f1d4d94bff0ef9c900389d16038077
SasankAVST <sganapathiraju@adaptavist.com>
2024-10-21 14:50:24 -0400

    chore(SD-1076): added unit tests for licenses v2

--- src/licenses/index.ts
+++ src/licenses/index.ts
@@ -1,54 +1,55 @@
 import { ApiRequest, TVersion, Version } from '..';
-import { CheckLicenseInput, CheckLicensesCapabilitiesResponse, CreateAdhocLicenseInput, GetAllLicensesResponse, GetLicenseCountInput, GetLicenseOptions, GetPurchasersLicensesInput, License, GetLicenseCountResponse, UpdateManyLicenseInput, GetGranteeIdLicensesInput } from '@/src/types';
+import { CheckLicenseInput, CheckLicensesCapabilitiesResponse, CreateAdhocLicenseInput, GetAllLicensesResponse, GetLicenseOptions, License, GetLicenseCountResponse, UpdateManyLicenseInput, GetLicenseCountOptions, GetPurchasersLicensesOptions } from '../../src/types';
 import { v2LicenseMethods } from './v2';
 
+// TODO: do not write docs for options, just link the docs
 export type LicenseVersions = {
   [Version.V2]: {
     /**
     *  Get all licenses
-    *
-    * @param {GetLicenseOptions} options
+    * 
+    * @param {GetLicenseOptions} options 
     * @param {GetLicenseOptions} options.status - The status of the licenses, can be one of: "ACTIVE" "CANCELED" "EVALUATION" "SCHEDULED" "TRIALING" "INACTIVE"
-    The status of the licenses.
+The status of the licenses.
     * @param {GetLicenseOptions} options.cursor - A unique identifier for the last item of one page to get the next page. This will be either the first or last property from the previous response. No cursor is required on the first request.
     * @param {GetLicenseOptions} options.take - Default: "20"
-    The number of records to take before or after cursor. A positive or negative in the range of -100 to 100. Use a number below 0 to take the previous records before the cursor.
+The number of records to take before or after cursor. A positive or negative in the range of -100 to 100. Use a number below 0 to take the previous records before the cursor.
     * @param {GetLicenseOptions} options.subscriptionUuid - Filters licenses by their subscription. Useful for returning licenses on a per seat subscription.
-    *
+    * 
     * @returns {Promise<GetAllLicensesResponse>} All licenses present on the account
     */
     getAll: (options?: GetLicenseOptions) => Promise<GetAllLicensesResponse>;
     /**
      *  Get one license
      *
-     *  @param {string} licenseUuid - The UUID of the license
+     *  @param {string} uuid - The UUID of the license
      *  @param {{ expand: string[] }} options
      *  @param {{ expand: string[] }} options.expand - Specify the resource fields to expand as comma separated values eg `expand=subscription,plan`
      *
      * @returns { Promise<License>}
      */
-    getOne: (licenseUuid: string, options?: { expand: string[] }) => Promise<License>;
+    getOne: (uuid: string, options?: { expand: string[] }) => Promise<License>;
     /**
      *  Get License's Count
      *
-     *  @param {GetLicenseCountInput} options
-     *  @param {GetLicenseCountInput} options.subscriptionUuid - The UUID of the subscription
-     *  @param {GetLicenseCountInput} options.status - The status of the licenses, can be one of: "ACTIVE" "CANCELED" "EVALUATION" "SCHEDULED" "TRIALING" "INACTIVE"
+     *  @param {GetLicenseCountOptions} options
+     *  @param {GetLicenseCountOptions} options.subscriptionUuid - (Optional) The UUID of the subscription
+     *  @param {GetLicenseCountOptions} options.status - (Optional) The status of the licenses, can be one of: "ACTIVE" "CANCELED" "EVALUATION" "SCHEDULED" "TRIALING" "INACTIVE"
      *
      * @returns {Promise<GetLicenseCountResponse>}
      */
-    getCount: (options: GetLicenseCountInput) => Promise<GetLicenseCountResponse>;
+    getCount: (options?: GetLicenseCountOptions) => Promise<GetLicenseCountResponse>;
     /**
      *  Get Purchasers Licenses
      *
-     *  @param {GetPurchasersLicensesInput} purchaserData
-     *  @param {GetPurchasersLicensesInput} purchaserData.purchaser - The purchaser of the licenses
-     *  @param {GetPurchasersLicensesInput} purchaserData.productUuid - The UUID of the product that the licenses are on
-     *  @param {GetPurchasersLicensesInput} purchaserData.status - The status of the licenses.
+     *  @param {GetPurchasersLicensesOptions} options
+     *  @param {GetPurchasersLicensesOptions} options.purchaser - The purchaser of the licenses
+     *  @param {GetPurchasersLicensesOptions} options.productUuid - The UUID of the product that the licenses are on
+     *  @param {GetPurchasersLicensesOptions} options.status - (Optional) The status of the licenses to filter by
      *
      * @returns {Promise<License[]>}
      */
-    getForPurchaser: (options: GetPurchasersLicensesInput) => Promise<License[]>;
+    getForPurchaser: (options: GetPurchasersLicensesOptions) => Promise<License[]>;
     /**
      *  Get licenses for granteeId
      *
@@ -58,16 +59,16 @@ export type LicenseVersions = {
      *
      * @returns {Promise<License[]>}
      */
-    getForGranteeId: (granteeId: string, options?: GetGranteeIdLicensesInput) => Promise<License[]>;
+    getForGranteeId: (granteeId: string, options?: { expand?: string[] }) => Promise<License[]>;
     /**
      *  Creates a single license or many licenses with the details provided
      *
-     * @param {CreateAdhocLicenseInput[]} licenseDetails - The details to create the new license with
-     * @param {CreateAdhocLicenseInput[]} licenseDetails.planUuid - The UUID of the plan associated with the license. The planUuid can be found on the Plan view in the Salable dashboard
-     * @param {CreateAdhocLicenseInput[]} licenseDetails.member - The ID of the member who will manage the license.
-     * @param {CreateAdhocLicenseInput[]} licenseDetails.granteeId - (Optional) The grantee ID for the license.
-     * @param {CreateAdhocLicenseInput[]} licenseDetails.status - (Optional) The status of the created license, e.g. "ACTIVE" "TRIALING"
-     * @param {CreateAdhocLicenseInput[]} licenseDetails.endTime - (Optional) Provide a custom end time for the license; this will override the plan's default interval.
+     * @param {CreateAdhocLicenseInput[]} data - The details to create the new license with
+     * @param {CreateAdhocLicenseInput[]} data.planUuid - The UUID of the plan associated with the license. The planUuid can be found on the Plan view in the Salable dashboard
+     * @param {CreateAdhocLicenseInput[]} data.member - The ID of the member who will manage the license.
+     * @param {CreateAdhocLicenseInput[]} data.granteeId - (Optional) The grantee ID for the license.
+     * @param {CreateAdhocLicenseInput[]} data.status - (Optional) The status of the created license, e.g. "ACTIVE" "TRIALING"
+     * @param {CreateAdhocLicenseInput[]} data.endTime - (Optional) Provide a custom end time for the license; this will override the plan's default interval.
      *
      * @returns {Promise<License | License[]>} The data for the new license or licenses created
      */
@@ -75,50 +76,50 @@ export type LicenseVersions = {
     /**
      *  Update a license
      *
-     * @param {string} licenseUuid - The UUID of the license
+     * @param {string} uuid - The UUID of the license
      * @param {{granteeId: string}} data
      * @param {{granteeId: string}} data.granteeId - The value of the new granteeId
      *
      * @returns {Promise<License>} The data of the updated license
      */
-    update: (licenseUuid: string, data: { granteeId: string }) => Promise<License>;
+    update: (uuid: string, data: { granteeId: string }) => Promise<License>;
     /**
      *  Update many license's
      *
-     * @param {UpdateManyLicenseInput[]} licenses - The config array of all the licenses you wish to update
-     * @param {UpdateManyLicenseInput[]} licenses.uuid - The UUID of the license to update
-     * @param {UpdateManyLicenseInput[]} licenses.granteeId - The new granteeId of the license
+     * @param {UpdateManyLicenseInput[]} data - The config array of all the licenses you wish to update
+     * @param {UpdateManyLicenseInput[]} data.uuid - The UUID of the license to update
+     * @param {UpdateManyLicenseInput[]} data.granteeId - The new granteeId of the license
      *
      * @returns {Promise<License[]>} The data of the updated license
      */
-    updateMany: (licenses: UpdateManyLicenseInput[]) => Promise<License[]>;
+    updateMany: (data: UpdateManyLicenseInput[]) => Promise<License[]>;
     /**
      *  Cancel a license
      *
-     * @param {string} licenseUuid - The UUID of the license
+     * @param {string} uuid - The UUID of the license
      *
      * @returns {Promise<void>}
      */
-    cancel: (licenseUuid: string) => Promise<void>;
+    cancel: (uuid: string) => Promise<void>;
     /**
      *  Cancel many licenses
      *
-     * @param {string[]} licenseUuids - Array of license uuids to be canceled
+     * @param {uuids} uuids - Array of license uuids to be canceled
      *
      * @returns {Promise<void>}
      */
-    cancelMany: (licenseUuids: string[]) => Promise<void>;
+    cancelMany: (data: { uuids: string[] }) => Promise<void>;
     /**
      *  Checks a license's capabilities
      *
-     * @param {CheckLicenseInput} checkData
-     * @param {CheckLicenseInput} checkData.productUuid - The UUID of the product to check the license against
-     * @param {CheckLicenseInput} checkData.granteeIds - The grantee IDs to check the license for
-     * @param {CheckLicenseInput} checkData.grace - (Optional) The number of days to extend the end dates of capabilities
+     * @param {CheckLicenseInput} options
+     * @param {CheckLicenseInput} options.productUuid - The UUID of the product to check the license against
+     * @param {CheckLicenseInput} options.granteeIds - The grantee IDs to check the license for
+     * @param {CheckLicenseInput} options.grace - (Optional) The number of days to extend the end dates of capabilities
      *
      * @returns {Promise<CheckLicensesCapabilitiesResponse>} The capabilities of the license passed
      */
-    check: (checkData: CheckLicenseInput) => Promise<CheckLicensesCapabilitiesResponse>;
+    check: (options: CheckLicenseInput) => Promise<CheckLicensesCapabilitiesResponse>;
     /**
      *  Verifies a license check
      *
@@ -128,7 +129,7 @@ export type LicenseVersions = {
      *
      * @returns {boolean} The result of the verification
      */
-    verify: ({ publicKey, signature, payload }: { publicKey: string; signature: string; payload: string }) => boolean;
+    verify: (options: { publicKey: string; signature: string; payload: string }) => boolean;
   };
 };
 
--- src/licenses/v2/licenses-v2.test.ts
+++ src/licenses/v2/licenses-v2.test.ts
@@ -1,152 +1,227 @@
-// // licenses-v2.test.ts
+// licenses-v2.test.ts
 
-// import { Version, initRequest } from "../..";
-// import { v2LicenseMethods } from ".";
-// import { log } from "console";
+import { Version, initRequest } from '../..';
+import { v2LicenseMethods } from '.';
+import { log } from 'console';
 
-// // test the various query params of each method
-// //  - grab the schemas from TPA
-// //  - grab the types from the TPA
-// //  - version the types
-// //  - convert interfaces to types
-// describe('Licenses V2 Tests', () => {
-//     const apiKey = process.env.SALABLE_TEST_API_KEY!;
-//     const version = Version.V2;
+describe('Licenses V2 Tests', () => {
+  const apiKey = process.env.SALABLE_TEST_API_KEY!;
+  const version = Version.V2;
 
-//     const req = initRequest(apiKey, version);
-//     const licensesV2 = v2LicenseMethods(req);
+  const req = initRequest(apiKey, version);
+  const licensesV2 = v2LicenseMethods(req);
 
-//     it('getAll: Should succesfully fetch licenses', async () => {
-//         const data = await licensesV2.getAll();
+  const cancellableLicenseUuid = '48cdd0ee-2954-436e-ad10-39cd418e5b2d';
+  const cancellableLicenseUuidTwo = '9868b62f-84af-46f9-8a3e-c43db31243ce';
+  const cancellableLicenseUuidThree = '67b913c2-2713-4d6a-a106-a44e773fc15d';
 
-//         expect(data).toEqual(
-//             expect.objectContaining({
-//                 first: expect.any(String),
-//                 last: expect.any(String),
-//                 data: expect.arrayContaining([licenseSchema])
-//             })
-//         );
-//     });
-//     it('getOne: Should succesfully fetch the specified license', async () => {
-//         const data = await licensesV2.getOne('1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c');
+  it('getAll: Should succesfully fetch licenses', async () => {
+    const data = await licensesV2.getAll();
+    const dataWithSearchParams = await licensesV2.getAll({ status: 'ACTIVE', take: '3', subscriptionUuid: '04c4bada-7133-4829-a27c-8e5b00558b9e' });
 
-//         log(data)
-//         expect(data).toEqual(expect.objectContaining(licenseSchema))
-//     });
-//     it('getCount: Should succesfully fetch a subscriptions count', async () => {
-//         const data = await licensesV2.getCount({ subscriptionUuid: '04c4bada-7133-4829-a27c-8e5b00558b9e', status: 'ACTIVE' });
+    expect(data.data.length).toEqual(11);
 
-//         expect(data).toEqual(expect.objectContaining({
-//             count: expect.any(Number),
-//             assigned: expect.any(Number),
-//             unassigned: expect.any(Number),
-//         }));
-//     });
-//     it('getForPurchaser: Should succesfully fetch a purchasers licenses', async () => {
-//         const data = await licensesV2.getForPurchaser({ purchaser: 'tester@testing.com', productUuid: '29c9a7c8-9a41-4e87-9e7e-7c62d293c131' });
+    expect(dataWithSearchParams).toEqual(
+      expect.objectContaining({
+        first: expect.any(String),
+        last: expect.any(String),
+        data: expect.arrayContaining([licenseSchema]),
+      }),
+    );
+    expect(dataWithSearchParams.data.length).toEqual(3);
+    for (const license of dataWithSearchParams.data) {
+      expect(license).toHaveProperty('status', 'ACTIVE');
+    }
+  });
 
-//     });
-//     it('getForGranteeId: Should succesfully fetch a grantees licenses', async () => {
-//         const data = await licensesV2.getForGranteeId({ granteeId: '123456' });
+  it('getOne: Should succesfully fetch the specified license', async () => {
+    const data = await licensesV2.getOne('1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c');
+    const dataWithSearchParams = await licensesV2.getOne('1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c', { expand: ['plan'] });
 
-//     });
-//     it('create: Should succesfully create a license', async () => {
-//         const data = await licensesV2.create([{
-//             planUuid: '5a866dba-20c9-466f-88ac-e05c8980c90b',
-//             member: 'example',
-//             granteeId: '123456',
-//             status: 'ACTIVE'
-//         }]);
+    expect(data).toEqual(expect.objectContaining(licenseSchema));
+    expect(data).not.toHaveProperty('plan');
 
-//         log(data)
+    expect(dataWithSearchParams).toEqual(expect.objectContaining(licenseSchema));
+    expect(dataWithSearchParams).toHaveProperty('plan', planSchema);
+  });
+  it('getCount: Should succesfully fetch a subscriptions count', async () => {
+    const data2 = await licensesV2.getCount();
 
-//     });
-//     it('update: Should succesfully update a license', async () => {
-//         const data = await licensesV2.update('1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c', { granteeId: 'updated-grantee-id' });
+    log(data2);
 
-//     });
-//     it('updateMany: Should succesfully update multiple licenses', async () => {
-//         const data = await licensesV2.updateMany([
-//             {
-//                 uuid: '1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c',
-//                 granteeId: 'updated-grantee'
-//             },
-//             {
-//                 uuid: '264e98fb-38c6-48cd-aa77-5b6b631fbc95',
-//                 granteeId: 'updated-grantee'
-//             }
-//         ]);
+    const data = await licensesV2.getCount({ subscriptionUuid: '04c4bada-7133-4829-a27c-8e5b00558b9e', status: 'ACTIVE' });
 
-//     });
-//     it('cancel: Should succesfully cancel the specified license', async () => {
-//         const data = await licensesV2.cancel('264e98fb-38c6-48cd-aa77-5b6b631fbc95');
+    expect(data).toEqual(
+      expect.objectContaining({
+        count: expect.any(Number),
+        assigned: expect.any(Number),
+        unassigned: expect.any(Number),
+      }),
+    );
+  });
+  it('getForPurchaser: Should succesfully fetch a purchasers licenses', async () => {
+    const data = await licensesV2.getForPurchaser({ purchaser: 'tester@testing.com', productUuid: '29c9a7c8-9a41-4e87-9e7e-7c62d293c131', status: 'ACTIVE' });
 
-//     });
-//     it('cancelMany: Should succesfully multiple licenses', async () => {
-//         const data = await licensesV2.cancelMany(['28362f68-d919-4a3a-bada-b868a104bd37', '80b36154-3d49-47b2-b99a-1c59698c14db']);
+    expect(data).toEqual(expect.arrayContaining([expect.objectContaining(licenseSchema)]));
+    for (const license of data) {
+      expect(license).toHaveProperty('purchaser', 'tester@testing.com');
+      expect(license).toHaveProperty('productUuid', '29c9a7c8-9a41-4e87-9e7e-7c62d293c131');
+      expect(license).toHaveProperty('status', 'ACTIVE');
+    }
+  });
+  it('getForGranteeId: Should succesfully fetch a grantees licenses', async () => {
+    const data = await licensesV2.getForGranteeId('123456', { expand: ['plan'] });
 
-//     });
-//     it('check: Should succesfully check the specified grantees permissions', async () => {
-//         const data = await licensesV2.check({
-//             productUuid: '29c9a7c8-9a41-4e87-9e7e-7c62d293c131',
-//             granteeIds: ['123456', 'userId_0']
-//         });
+    expect(data).toEqual(expect.arrayContaining([expect.objectContaining(licenseSchema)]));
+    for (const license of data) {
+      expect(license).toHaveProperty('plan', planSchema);
+    }
+  });
+  it('create: Should succesfully create a license', async () => {
+    const data = await licensesV2.create([
+      {
+        planUuid: '5a866dba-20c9-466f-88ac-e05c8980c90b',
+        member: 'example',
+        granteeId: '123456',
+        status: 'ACTIVE',
+        endTime: '2025-07-06T12:00:00.000Z',
+      },
+    ]);
 
-//     });
-//     it('verify: Verifies the license-check signatures correctly', async () => {
-//         const testPublicKeyPem = `-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAES7jvFxC50Fe2hHd3Sn7Q8TvnxuSZ\nV8HvRHGDvFacOiESAqg3uroeNTgoT7lD4BwQ+fFsn7zig5hwncoTsrCPbw==\n-----END PUBLIC KEY-----`;
-//         const testLicenseCheckData = [
-//             { capability: 'One', expiry: '2024-08-14T13:15:49.310Z' },
-//             { capability: 'Two', expiry: '2024-08-14T13:15:49.310Z' },
-//             { capability: 'free_plan_name', expiry: '2024-08-14T13:32:29.313Z' },
-//             { capability: 'Three', expiry: '2024-08-14T13:32:29.313Z' },
-//             { capability: 'Four', expiry: '2024-08-14T13:32:29.313Z' },
-//         ];
-//         const testSignature =
-//             '3045022100b210aa29519f3146afe7a0d343a6b7ec5e47a1ac0de9686e2ec4cf0081e159c402206ecf98ad4d1d339c59f7ff3b4744d1f377747702c6253f7904ef6589191a2254';
-//         const testIncorrectSignature = 'bad-signature';
+    expect(data).toEqual(expect.arrayContaining([expect.objectContaining(licenseSchema)]));
+  });
+  it('update: Should succesfully update a license', async () => {
+    const data = await licensesV2.update('1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c', { granteeId: 'updated-grantee-id' });
 
-//         const falseLicenseCheck = licensesV2.verify({
-//             publicKey: testPublicKeyPem,
-//             signature: testIncorrectSignature,
-//             payload: JSON.stringify(testLicenseCheckData)
-//         });
-//         const trueLicenseCheck = licensesV2.verify({
-//             publicKey: testPublicKeyPem,
-//             signature: testSignature,
-//             payload: JSON.stringify(testLicenseCheckData)
-//         });
+    expect(data.granteeId).toEqual('updated-grantee-id');
+  });
+  it('updateMany: Should succesfully update multiple licenses', async () => {
+    const data = await licensesV2.updateMany([
+      {
+        uuid: '1e97a9ea-c66a-4822-a5f1-bebf5ea7e44c',
+        granteeId: 'updated-grantee',
+      },
+      {
+        uuid: '264e98fb-38c6-48cd-aa77-5b6b631fbc95',
+        granteeId: 'updated-grantee',
+      },
+    ]);
 
-//         expect(falseLicenseCheck).toEqual(false);
-//         expect(trueLicenseCheck).toEqual(true);
-//     });
-// });
+    for (const license of data) {
+      expect(license.granteeId).toEqual('updated-grantee');
+    }
+  });
+  it('cancel: Should succesfully cancel the specified license', async () => {
+    const data = await licensesV2.cancel(cancellableLicenseUuid);
 
-// const licenseCapabilitySchema = { // check type
-//     uuid: expect.any(String),
-//     productUuid: expect.any(String),
-//     name: expect.any(String),
-//     status: expect.any(String),
-//     description: expect.toBeOneOf([expect.any(String), null]),
-//     updatedAt: expect.any(String),
-// };
+    expect(data).toEqual(undefined);
+  });
+  it('cancelMany: Should succesfully multiple licenses', async () => {
+    const data = await licensesV2.cancelMany({ uuids: [cancellableLicenseUuidTwo, cancellableLicenseUuidThree] });
 
-// const licenseSchema = {
-//     uuid: expect.any(String),
-//     name: expect.toBeOneOf([expect.any(String), null]),
-//     email: expect.toBeOneOf([expect.any(String), null]),
-//     subscriptionUuid: expect.any(String),
-//     status: 'ACTIVE',
-//     granteeId: expect.any(String) || null,
-//     paymentService: 'ad-hoc',
-//     purchaser: expect.any(String),
-//     type: 'metered',
-//     productUuid: expect.any(String),
-//     planUuid: expect.any(String),
-//     capabilities: expect.arrayContaining([licenseCapabilitySchema]),
-//     metadata: null,
-//     startTime: expect.any(String),
-//     endTime: expect.any(String),
-//     updatedAt: expect.any(String),
-//     isTest: expect.any(Boolean)
-// };
+    expect(data).toEqual(undefined);
+  });
+  it('check: Should succesfully check the specified grantees permissions', async () => {
+    const data = await licensesV2.check({
+      productUuid: '29c9a7c8-9a41-4e87-9e7e-7c62d293c131',
+      granteeIds: ['123456'],
+    });
+
+    expect(data).toEqual(
+      expect.objectContaining({
+        capabilities: expect.arrayContaining([
+          {
+            capability: expect.any(String),
+            expiry: expect.any(String),
+          },
+        ]),
+        signature: expect.any(String),
+      }),
+    );
+  });
+  it('verify: Verifies the license-check signatures correctly', async () => {
+    const testPublicKeyPem = `-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAES7jvFxC50Fe2hHd3Sn7Q8TvnxuSZ\nV8HvRHGDvFacOiESAqg3uroeNTgoT7lD4BwQ+fFsn7zig5hwncoTsrCPbw==\n-----END PUBLIC KEY-----`;
+    const testLicenseCheckData = [
+      { capability: 'One', expiry: '2024-08-14T13:15:49.310Z' },
+      { capability: 'Two', expiry: '2024-08-14T13:15:49.310Z' },
+      { capability: 'free_plan_name', expiry: '2024-08-14T13:32:29.313Z' },
+      { capability: 'Three', expiry: '2024-08-14T13:32:29.313Z' },
+      { capability: 'Four', expiry: '2024-08-14T13:32:29.313Z' },
+    ];
+    const testSignature = '3045022100b210aa29519f3146afe7a0d343a6b7ec5e47a1ac0de9686e2ec4cf0081e159c402206ecf98ad4d1d339c59f7ff3b4744d1f377747702c6253f7904ef6589191a2254';
+    const testIncorrectSignature = 'bad-signature';
+
+    const falseLicenseCheck = licensesV2.verify({
+      publicKey: testPublicKeyPem,
+      signature: testIncorrectSignature,
+      payload: JSON.stringify(testLicenseCheckData),
+    });
+    const trueLicenseCheck = licensesV2.verify({
+      publicKey: testPublicKeyPem,
+      signature: testSignature,
+      payload: JSON.stringify(testLicenseCheckData),
+    });
+
+    expect(falseLicenseCheck).toEqual(false);
+    expect(trueLicenseCheck).toEqual(true);
+  });
+});
+
+const licenseCapabilitySchema = {
+  uuid: expect.any(String),
+  productUuid: expect.any(String),
+  name: expect.any(String),
+  status: expect.any(String),
+  description: expect.toBeOneOf([expect.any(String), null]),
+  updatedAt: expect.any(String),
+};
+
+const licenseSchema = {
+  uuid: expect.any(String),
+  name: expect.toBeOneOf([expect.any(String), null]),
+  email: expect.toBeOneOf([expect.any(String), null]),
+  subscriptionUuid: expect.toBeOneOf([expect.any(String), null]),
+  status: expect.toBeOneOf(['ACTIVE', 'CANCELED', 'EVALUATION', 'SCHEDULED', 'TRIALING', 'INACTIVE']),
+  granteeId: expect.toBeOneOf([expect.any(String), null]),
+  paymentService: expect.toBeOneOf(['ad-hoc', 'salable', 'stripe_existing']),
+  purchaser: expect.any(String),
+  type: expect.toBeOneOf(['licensed', 'metered', 'perSeat', 'customId', 'user']),
+  productUuid: expect.any(String),
+  planUuid: expect.any(String),
+  capabilities: expect.arrayContaining([licenseCapabilitySchema]),
+  metadata: expect.toBeOneOf([expect.anything(), null]),
+  startTime: expect.any(String),
+  endTime: expect.any(String),
+  updatedAt: expect.any(String),
+  isTest: expect.any(Boolean),
+};
+
+const planSchema = {
+  uuid: expect.any(String),
+  name: expect.any(String),
+  slug: expect.any(String),
+  description: expect.toBeOneOf([expect.any(String), null]),
+  displayName: expect.any(String),
+  status: expect.any(String),
+  trialDays: expect.toBeOneOf([expect.any(Number), null]),
+  evaluation: expect.any(Boolean),
+  evalDays: expect.any(Number),
+  perSeatAmount: expect.any(Number),
+  maxSeatAmount: expect.any(Number),
+  organisation: expect.any(String),
+  visibility: expect.any(String),
+  licenseType: expect.any(String),
+  hasAcceptedTransaction: expect.any(Boolean),
+  interval: expect.any(String),
+  length: expect.any(Number),
+  active: expect.any(Boolean),
+  planType: expect.any(String),
+  pricingType: expect.any(String),
+  environment: expect.any(String),
+  isTest: expect.any(Boolean),
+  paddlePlanId: null,
+  productUuid: expect.any(String),
+  salablePlan: expect.any(Boolean),
+  updatedAt: expect.any(String),
+  features: expect.toBeOneOf([expect.anything(), undefined]),
+};
