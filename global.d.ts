542341cc6818f0017a2d0df332f2d68454bfde61
Joshua Okoro <jokoro@adaptavist.com>
2024-10-23 11:34:47 -0500

    test: test suite for pricing table resource
    - testing and validating the schema for retrieving a pricing table

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,2 @@
 import 'jest-fetch-mock';
+import 'jest-extended';
--- src/pricing-tables/index.ts
+++ src/pricing-tables/index.ts
@@ -6,32 +6,23 @@ export type PricingTableVersions = {
   [Version.V2]: {
     /**
      *  Retrieves a pricing table by its UUID. This returns all necessary data on a Pricing Table to be able to display it.
-     *  @param {string} pricingTableUuid - The UUID for the pricingTable
-     *  @param {{ granteeId?: string; currency?: string;}} options
-     *  @param {{ granteeId?: string; currency?: string;}} options.granteeId - The unique identifier for the grantee
-     *  @param {{ granteeId?: string; currency?: string;}} options.currency - Uses the currency short name e.g. USD, defaults to the default currency on the Product which the Plan is linked to.
      *
-     * @returns {Promise<Product>} The data of the product requested
+     *  @param {string} pricingTableUuid - The UUID for the pricingTable
+     *  @param {{ granteeId?: string; currency?: string;}} options - (Optional) Filter parameters. See https://docs.salable.app/api/v2#tag/Pricing-Tables/operation/getPricingTableByUuid
+     *
+     * @returns {Promise<PricingTableResponse>}
      */
-    getOne: (
-      pricingTableUuid: string,
-      options?: { granteeId?: string; currency?: string },
-    ) => Promise<PricingTableResponse>;
+    getOne: (pricingTableUuid: string, options?: { granteeId?: string; currency?: string }) => Promise<PricingTableResponse>;
   };
 };
 
-export type PricingTableVersionedMethods<V extends TVersion> = V extends keyof PricingTableVersions
-    ? PricingTableVersions[V]
-    : never;
+export type PricingTableVersionedMethods<V extends TVersion> = V extends keyof PricingTableVersions ? PricingTableVersions[V] : never;
 
-export const PricingTablesInit = <V extends TVersion>(
-    version: V,
-    request: ApiRequest,
-): PricingTableVersionedMethods<V> => {
-    switch (version) {
-        case Version.V2:
-            return v2PricingTableMethods(request) as PricingTableVersionedMethods<V>;
-        default:
-            throw new Error('Unsupported version');
-    }
+export const PricingTablesInit = <V extends TVersion>(version: V, request: ApiRequest): PricingTableVersionedMethods<V> => {
+  switch (version) {
+    case Version.V2:
+      return v2PricingTableMethods(request) as PricingTableVersionedMethods<V>;
+    default:
+      throw new Error('Unsupported version');
+  }
 };
