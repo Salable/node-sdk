4ff770a9e92992ef1da5359609f01fc43dda5c1c
SasankAVST <sganapathiraju@adaptavist.com>
2024-10-30 10:20:21 -0400

    chore: install dependencies for unit tests

---
+++ test-utils/kms/kms-symmetric-encrypt.ts
@@ -0,0 +1,37 @@
+import { EncryptCommand, EncryptCommandInput, KMSClient } from '@aws-sdk/client-kms';
+
+export default async function kmsSymmetricEncrypt(data: string): Promise<Uint8Array> {
+  if (!process.env.AWS_THIRD_PARTY_API_REGION) throw Error('Missing environment variable AWS_THIRD_PARTY_API_REGION');
+  if (!process.env.AWS_ACCESS_KEY_ID) throw Error('Missing environment variable AWS_ACCESS_KEY_ID');
+  if (!process.env.AWS_SECRET_ACCESS_KEY) throw Error('Missing environment variable AWS_SECRET_ACCESS_KEY');
+  if (!process.env.AWS_SESSION_TOKEN) throw Error('Missing environment variable AWS_SESSION_TOKEN');
+
+  const kmsClient = new KMSClient({
+    region: process.env.AWS_THIRD_PARTY_API_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      sessionToken: process.env.AWS_SESSION_TOKEN,
+    },
+  });
+
+  const input: EncryptCommandInput = {
+    KeyId: process.env.KMS_ENCRYPTION_KEY_ARN,
+    Plaintext: new TextEncoder().encode(data),
+  };
+
+  const command = new EncryptCommand(input);
+  let encryptedData: Uint8Array | undefined;
+
+  try {
+    const response = await kmsClient.send(command);
+    encryptedData = response.CiphertextBlob;
+  } catch (err) {
+    console.log('KMS encryption error: ', err);
+    throw new Error('Failed to encrypt data');
+  }
+
+  if (!encryptedData) throw new Error('Failed to encrypt data');
+
+  return encryptedData;
+}
