c656bb06f4916177cabcba935708980e5b40ac83
SasankAVST <sganapathiraju@adaptavist.com>
2024-11-18 09:29:26 -0500

    chore: rm status from errors + add 404 error handling

--- src/exceptions/salable-error.ts
+++ src/exceptions/salable-error.ts
@@ -22,28 +22,26 @@ export type ResponseError = {
 };
 
 export class SalableError extends Error {
-  status: number;
   code: string;
   data?: Record<string, unknown> | null;
 
-  constructor(code: ErrorCodes, status: number, data?: Record<string, unknown> | null) {
+  constructor(code: ErrorCodes, data?: Record<string, unknown> | null) {
     super(JSON.stringify({ data }));
     this.code = code;
     this.data = data;
-    this.status = status;
   }
 }
 
 export class SalableResponseError extends SalableError {
-  constructor(code: ErrorCodes, status: number, data?: ResponseError) {
-    super(code, status, data);
+  constructor(code: ErrorCodes, data?: ResponseError) {
+    super(code, data);
     Object.setPrototypeOf(this, SalableResponseError.prototype);
   }
 }
 
 export class SalableValidationError extends SalableError {
-  constructor(code: ErrorCodes, status: number, data: ValidationError) {
-    super(code, status, data);
+  constructor(code: ErrorCodes, data: ValidationError) {
+    super(code, data);
     Object.setPrototypeOf(this, SalableValidationError.prototype);
   }
 }
